391cc993a3b4783487878a4adcbad51e13f4caae
davidmozart <davidmozart01@gmail.com>
2025-10-13 15:16:16 -0300

    Enviando POS para outra URL

--- authors/urls.py
+++ authors/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
     path('register/', views.regiter_view, name='register'),
+    path('register/create/', views.regiter_create, name='create'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import RegisterForm
+from django.http import Http404
+
+
 # Create your views here.
 
 def regiter_view(request):
-    form = RegisterForm()
+    
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+
     return render(request, 'authors/pages/register_view.html',{
         'form': form,
         })
+
+def regiter_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST
+    request.session['register_from_data'] = POST
+    form = RegisterForm(POST) 
+
+    return redirect('authors:register')
+    
